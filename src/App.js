4de620052a7f2d632dfe8235711de4cb3dc6df7d
rummanhase <rummanhase@gmail.com>
2023-04-23 16:17:16 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  function fetchFlickrData(apiKey, searchText, callback) {
+    const apiUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchText}&format=json&nojsoncallback=1`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.stat === "ok") {
+          const photos = data.photos.photo.map((photo) => ({
+            id: photo.id,
+            title: photo.title,
+            url: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+          }));
+          callback(null, photos);
+        } else {
+          callback(new Error(data.message), null);
+        }
+      })
+      .catch((error) => {
+        callback(error, null);
+      });
+  }
+  const apiKey = "e005d12081626241211a1d27ce3055ad";
+  const searchText = "cricket";
+
+  const [data, setData] = useState([]);
+  useEffect(
+    () =>
+      fetchFlickrData(apiKey, searchText, (error, photos) => {
+        if (error) {
+          console.error(error);
+        } else {
+          setData(photos);
+          console.log(photos);
+        }
+      }),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      React
+      {data.map((a, b) => (
+        <div key={a.id}>
+          <img src={a.url} alt={a.title} />
+        </div>
+      ))}
     </div>
   );
 }
